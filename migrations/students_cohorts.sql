9564db32b614211005b40429d5e36a95a27a1dba
Momowaz <momo.waz1593@gmail.com>
2023-05-22 13:34:57 -0700

    add assignmnet and assignment_submission tables and data

--- migrations/students_cohorts.sql
+++ migrations/students_cohorts.sql
@@ -4,14 +4,3 @@ CREATE TABLE cohorts (
   start_date DATE,
   end_date DATE
 );
-
-CREATE TABLE students (
-  id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255) NOT NULL,
-  email VARCHAR(255),
-  phone VARCHAR(32),
-  github VARCHAR(255),
-  start_date DATE,
-  end_date DATE,
-  cohort_id INTEGER REFERENCES cohorts(id) ON DELETE CASCADE
-);

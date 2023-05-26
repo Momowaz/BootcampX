9965edbc2e31f772f421c9efe5e1a96c10700401
Momowaz <momo.waz1593@gmail.com>
2023-05-26 00:21:34 -0700

    Update students.js and teachers.js to use parameterized queries

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -1,21 +1,37 @@
-const { Pool } = require('pg');
+const { Client } = require('pg');
 
-const pool = new Pool({
+const client = new Client({
   user: 'postgres',
   password: '123',
   host: 'localhost',
   database: 'bootcampx'
 });
 
-pool.query(`
-SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+const cohortName = process.argv[2] || 'JUL02';
+const limit = process.argv[3] || 5;
+
+const query = `
+SELECT students.id, students.name AS student_name, cohorts.name AS cohort_name
 FROM students
-JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
-`)
-.then(res => {
-  res.rows.forEach(user => {
-    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
-  })
-}).catch(err => console.error('query error', err.stack));
+JOIN cohorts ON cohorts.id = students.cohort_id
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`;
+
+const values = [`%${cohortName}%`, limit];
+
+async function runQuery() {
+  try {
+    await client.connect();
+    const res = await client.query(query, values);
+    res.rows.forEach(student => {
+      console.log(`${student.student_name} has an id of ${student.id} and is in the ${student.cohort_name} cohort`);
+    });
+  } catch (err) {
+    console.error('Error executing query', err);
+  } finally {
+    await client.end();
+  }
+}
+
+runQuery();
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -1,23 +1,38 @@
-const { Pool } = require('pg');
+const { Client } = require('pg');
 
-const pool = new Pool({
+const client = new Client({
   user: 'postgres',
   password: '123',
   host: 'localhost',
   database: 'bootcampx'
 });
 
-pool.query(`
-SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+const cohortName = process.argv[2] || 'JUL02';
+
+const query = `
+SELECT DISTINCT teachers.name AS teacher_name, cohorts.name AS cohort_name
 FROM teachers
-JOIN assistance_requests ON teacher_id = teachers.id
-JOIN students ON student_id = students.id
-JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
-ORDER BY teacher;
-`)
-.then(res => {
-  res.rows.forEach(row => {
-    console.log(`${row.cohort}: ${row.teacher}`);
-  })
-});
+JOIN assistance_requests ON teachers.id = assistance_requests.teacher_id
+JOIN students ON students.id = assistance_requests.student_id
+JOIN cohorts ON cohorts.id = students.cohort_id
+WHERE cohorts.name LIKE $1
+ORDER BY teacher_name;
+`;
+
+const values = [`%${cohortName}%`];
+
+async function runQuery() {
+  try {
+    await client.connect();
+    const res = await client.query(query, values);
+    res.rows.forEach(teacher => {
+      console.log(`${teacher.teacher_name} is the teacher for the ${teacher.cohort_name} cohort`);
+    });
+  } catch (err) {
+    console.error('Error executing query', err);
+  } finally {
+    await client.end();
+  }
+}
+
+runQuery();
